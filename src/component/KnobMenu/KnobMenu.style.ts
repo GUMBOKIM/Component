c68b0fe9a1892188b12a232c548450406d8b8824
daeheekim <dae4805@naver.com>
2023-05-22 00:17:32 +0900

    KnobMenu 기능 완성

--- src/component/KnobMenu/KnobMenu.style.ts
+++ src/component/KnobMenu/KnobMenu.style.ts
@@ -1,36 +1,82 @@
-import styled from "styled-components";
+import styled, {css} from "styled-components";
 
 export const Container = styled.div`
   position: relative;
   width: 400px;
   height: 400px;
-  margin: 100px;
 `;
 
-export const Circle = styled.ul`
-  position: relative;
+export const Circle = styled.ul<{ isActive: boolean }>`
+  position: absolute;
+  top: 0;
+  left: 0;
+
   width: 100%;
   height: 100%;
   border-radius: 50%;
-  background-color: gray;
+  background-color: black;
 
-  -webkit-user-select:none;
-  -moz-user-select:none;
-  -ms-user-select:none;
-  user-select:none
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+
+  z-index: 100;
+  
+  //transition: transform 0.25s ease-in;
+
+  ${({isActive}) => {
+    return isActive ?
+            css`
+              display: block;
+              & > * {
+                display: block;
+              }
+            ` : 
+            css`
+              display: none;
+              & > * {
+                display: none;
+              }
+            `;
+  }}
+`
+
+export const InnerCircle = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  width: 30%;
+  height: 30%;
+
+  border-radius: 50%;
+  background-color: dimgray;
+
+  z-index: 200;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `
 
 export const Item = styled.li`
   position: absolute;
-  width: 40px;
-  height: 40px;
+  width: 50px;
+  height: 50px;
   transform: translate(-50%, -50%);
-  background-color: blue;
-  transform-origin: 50% 50%;
+  border-radius: 50%;
+  background-color: inherit;
+
+  &:hover {
+    background-color: gray;
+    transition: background-color 0.25s ease-in;
+  }
 `
 
 export const VerticalLine = styled.div`
-    position: absolute;
+  position: absolute;
   top: 50%;
   width: 100%;
   height: 1px;
@@ -50,9 +96,17 @@ export const ItemLine = styled.div`
   position: absolute;
   top: 25%;
   left: 25%;
-  
+
   width: 50%;
   height: 50%;
   border-radius: 50%;
   border: 1px black solid;
 `
+
+export const Button = styled.button`
+  width: 100%;
+  height: 100%;
+
+  color: white;
+`;
+
--- src/component/KnobMenu/KnobMenu.tsx
+++ src/component/KnobMenu/KnobMenu.tsx
@@ -1,74 +1,83 @@
 import * as S from "./KnobMenu.style";
-import {useEffect, useRef} from "react";
+import {useEffect, useLayoutEffect, useRef, useState} from "react";
 import {calculateDegree, Location} from "./KnobMenu.util";
 
 const KnobMenu = () => {
+    const [isActive, setIsActive] = useState(true);
+
     const circleRef = useRef<HTMLUListElement>(null);
 
     useEffect(() => {
-        const circle = circleRef.current;
-        if (circle) {
-            const {x: refX, y: refY,  width} = circle.getBoundingClientRect();
-            const r = width / 2;
-            const circleCenter: Location = {x: refX + r, y: refY + r};
+        if (isActive) {
+            const circle = circleRef.current;
+            if (circle) {
+                const {x: refX, y: refY, width} = circle.getBoundingClientRect();
+                const r = width / 2;
+                const circleCenter: Location = {x: refX + r, y: refY + r};
 
-            const distanceCenterFromItem = r / 2;
+                const distanceCenterFromItem = 2 * r / 3;
 
-            const childCount = circle.childElementCount;
-            const childSize = {width: circle.children[0].clientWidth, height: circle.children[0].clientHeight}
-            // ITEM 정렬
-            for (let i = 0; i < childCount; i++) {
-                const item = circle.children[i] as HTMLLIElement;
-                const x = r - Math.cos(i / childCount * Math.PI * 2) * distanceCenterFromItem;
-                const y = r + Math.sin(i / childCount * Math.PI * 2) * distanceCenterFromItem;
-                item.style.top = (x - childSize.width / 2) + "px";
-                item.style.left = (y - childSize.height / 2) + "px";
-                item.style.transform = `rotate(${i / childCount * 360}deg)`
+                const childCount = circle.childElementCount;
+                const childSize = {width: circle.children[0].clientWidth, height: circle.children[0].clientHeight}
+                // ITEM 정렬
+                for (let i = 0; i < childCount; i++) {
+                    const item = circle.children[i] as HTMLLIElement;
+                    const x = r - Math.cos(-Math.PI / 2 - i / childCount * Math.PI * 2) * distanceCenterFromItem;
+                    const y = r + Math.sin(-Math.PI / 2 - i / childCount * Math.PI * 2) * distanceCenterFromItem;
+                    item.style.transform = `translate(${x - childSize.width / 2}px, ${y - childSize.height / 2}px) rotate(${i / childCount * 360}deg)`
+                }
+
+                // 회전 이벤트
+                let circleRotatedDegree = 0;
+                let startRad = 0;
+
+                const mouseMoveEvent = (e: MouseEvent) => {
+                    const nowDegree = calculateDegree(circleCenter, {x: e.clientX, y: e.clientY});
+                    const moveDegree = nowDegree - startRad;
+                    circle.style.rotate = `${circleRotatedDegree + moveDegree}deg`
+
+                    e.stopPropagation();
+                }
+                circle.addEventListener('mousedown', (e) => {
+                    startRad = calculateDegree(circleCenter, {x: e.clientX, y: e.clientY});
+
+                    circle.addEventListener('mousemove', mouseMoveEvent)
+                });
+
+                circle.addEventListener('mouseup', (e) => {
+                    const nowDegree = calculateDegree(circleCenter, {x: e.clientX, y: e.clientY});
+                    const moveRad = nowDegree - startRad;
+                    circleRotatedDegree = circleRotatedDegree + moveRad;
+                    circle.removeEventListener('mousemove', mouseMoveEvent)
+                })
+
+                circle.addEventListener('mouseleave', (e) => {
+                    const nowDegree = calculateDegree(circleCenter, {x: e.clientX, y: e.clientY});
+                    const moveRad = nowDegree - startRad;
+                    circleRotatedDegree = circleRotatedDegree + moveRad;
+                    circle.removeEventListener('mousemove', mouseMoveEvent)
+                })
+
             }
-
-            // 회전 이벤트
-            let circleRotatedDegree = 0;
-            let startRad = 0;
-
-            const mouseMoveEvent = (e: MouseEvent) => {
-                const nowDegree = calculateDegree(circleCenter, {x: e.clientX, y: e.clientY});
-                const moveDegree = nowDegree - startRad;
-                circle.style.transform = `rotate(${circleRotatedDegree + moveDegree}deg)`
-
-                e.stopPropagation();
-            }
-            circle.addEventListener('mousedown', (e) => {
-                startRad = calculateDegree(circleCenter, {x: e.clientX, y: e.clientY});
-
-                circle.addEventListener('mousemove', mouseMoveEvent)
-            });
-
-            circle.addEventListener('mouseup', (e) => {
-                const nowDegree = calculateDegree(circleCenter, {x: e.clientX, y: e.clientY});
-                const moveRad = nowDegree - startRad;
-                circleRotatedDegree = circleRotatedDegree + moveRad;
-                circle.removeEventListener('mousemove', mouseMoveEvent)
-            })
-
         }
-    }, []);
+    }, [isActive]);
 
     return (
         <S.Container>
-            <S.Circle ref={circleRef}>
+            <S.Circle isActive={isActive} ref={circleRef}>
                 <>
                     {[1, 2, 3, 4, 5, 6].map(item =>
                         <S.Item key={item}>
-                            <button onClick={() => alert(`item${item} click!!`)}>
+                            <S.Button onClick={() => alert(`item${item} click!!`)}>
                                 Item{item}
-                            </button>
+                            </S.Button>
                         </S.Item>
                     )}
                 </>
             </S.Circle>
-            <S.VerticalLine/>
-            <S.HorizonLine/>
-            <S.ItemLine/>
+            <S.InnerCircle onClick={() => setIsActive(prev => !prev)}>
+                {isActive ? 'CLOSE' : 'OPEN'}
+            </S.InnerCircle>
         </S.Container>
     )
 }
